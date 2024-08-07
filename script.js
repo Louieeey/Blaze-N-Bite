8ca3e55f1bb942e6769935cfec910b1e64d920b5
Bryan Maaño <louiebryanmaano@gmail.com>
2024-08-08 05:17:50 +0800

    nodejs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.console.log("Hello world");
+
 document.addEventListener('DOMContentLoaded', function() {
     const slides = document.querySelector('.slider'); // Selecting the slider container
     const manualNavButtons = document.querySelectorAll('.manual-btn'); // Selecting all manual navigation buttons
@@ -30,22 +32,3 @@ document.addEventListener('DOMContentLoaded', function() {
     // Automatic slide navigation
     setInterval(nextSlide, 5000); // Setting interval to automatically transition to the next slide every 5 seconds
 });
-
-// modal
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
-}
-
-// Close the modal when clicking outside of it
-window.onclick = function(event) {
-    const modals = document.getElementsByClassName('modal');
-    for (let i = 0; i < modals.length; i++) {
-        if (event.target === modals[i]) {
-            modals[i].style.display = "none";
-        }
-    }
-}
